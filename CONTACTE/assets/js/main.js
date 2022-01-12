54dd352ad90711e3f51a8f208fa6c82882ee0021
Ruben/ <rubencontactpro@bluewin.ch>
2022-01-12 08:31:35 +0100

    Change all

--- CONTACTE/assets/js/main.js
+++ CONTACTE/assets/js/main.js
@@ -1,19 +1,27 @@
+
 /*===== MENU SHOW Y HIDDEN =====*/
+
 const navMenu = document.getElementById('nav-menu'),
     toggleMenu = document.getElementById('nav-toggle'),
     closeMenu = document.getElementById('nav-close')
 
+
 // SHOW
+
 toggleMenu.addEventListener('click', ()=>{
     navMenu.classList.toggle('show')
 })
 
+
 // HIDDEN
+
 closeMenu.addEventListener('click', ()=>{
     navMenu.classList.remove('show')
 })
 
+
 /*===== MOUSEMOVE HOME IMG =====*/
+
 document.addEventListener('mousemove', move);
 function move(e){
     this.querySelectorAll('.move').forEach(layer =>{
@@ -26,7 +34,9 @@ function move(e){
     })
 }
 
+
 /*===== GSAP ANIMATION =====*/
+
 // NAV
 gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay:2, y: 10})
 gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,})
